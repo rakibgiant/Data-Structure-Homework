4684f90abd0d349635a5d4a30248b2cbdaddaa60
Md Rakibul Islam <55613010+rakibgiant@users.noreply.github.com>
2020-06-20 10:25:37 +0800

    Linked list insertion

--- main.c
+++ main.c
@@ -1,45 +1,111 @@
 #include <stdio.h>
 #include <stdlib.h>
-typedef struct node Node;
-struct node {
-    int data;
-    Node* next;
-};
-Node *head;
-//head = 0;
-Node *create_node(int item,Node *next)
+
+struct node
 {
-    for(int item=0;item<500;item++){
-    Node *new_node = (Node *)malloc(sizeof(Node));
-    if (new_node == NULL){  //if there are any error new_node will assign NULL
-        printf("Error! Could Not Create A New Node\n");
-        exit(1);
-    }else{
-            new_node->data = item;
-    new_node->next = next;
-        printf("data = %d\n",new_node->data);
-         }
+    int num;                    //Data of the node
+    struct node *nextptr;       //Address of the node
+}*stnode;
 
-    }
-    for(int item=501;item<1000;item++){
-    Node *new_node = (Node *)malloc(sizeof(Node));
-    if (new_node == NULL){  //if there are any error new_node will assign NULL
-        printf("Error! Could Not Create A New Node\n");
-        exit(1);
-    }else{
-            new_node->data = item;
-    new_node->next = next;
-        printf("data = %d\n",new_node->data);
-         }
-
-    }
-
-    return 0;
-}
+void createNodeList(int n);     //function to create the list
+void reverseDispList();         //function to convert the list in reverse
+void displayList();             //function to display the list
 
 int main()
 {
-create_node(0,1);
+    int n;
+		printf("\n\n Linked List : Create a singly linked list and print it in reverse order :\n");
 
+
+    printf(" Input the number of nodes : ");
+    scanf("%d", &n);
+    createNodeList(n);
+    printf("\n Data entered in the list are : \n");
+    displayList();
+    reverseDispList();
+    printf("\n The list in reverse are :  \n");
+    displayList();
     return 0;
 }
+
+void createNodeList(int n)
+{
+    struct node *fnNode, *tmp;
+    int num, i;
+    stnode = (struct node *)malloc(sizeof(struct node));
+    if(stnode == NULL) //check whether the stnode is NULL and if so no memory allocation
+    {
+        printf(" Memory can not be allocated.");
+    }
+    else
+    {
+// reads data for the node through keyboard
+        printf(" Input data for node 1 : ");
+        scanf("%d", &num);
+        stnode-> num = num;
+        stnode-> nextptr = NULL; //Links the address field to NULL
+        tmp = stnode;
+//Creates n nodes and adds to linked list
+        for(i=2; i<=n; i++)
+        {
+            fnNode = (struct node *)malloc(sizeof(struct node));
+            if(fnNode == NULL) //check whether the fnnode is NULL and if so no memory allocation
+            {
+                printf(" Memory can not be allocated.");
+                break;
+            }
+            else
+            {
+                printf(" Input data for node %d : ", i);
+                scanf(" %d", &num);
+                fnNode->num = num;      // links the num field of fnNode with num
+                fnNode->nextptr = NULL; // links the address field of fnNode with NULL
+                tmp->nextptr = fnNode; // links previous node i.e. tmp to the fnNode
+                tmp = tmp->nextptr;
+            }
+        }
+    }
+}
+
+void reverseDispList()
+{
+    struct node *prevNode, *curNode;
+
+    if(stnode != NULL)
+    {
+        prevNode = stnode;
+        curNode = stnode->nextptr;
+        stnode = stnode->nextptr;
+
+        prevNode->nextptr = NULL; //convert the first node as last
+
+        while(stnode != NULL)
+        {
+            stnode = stnode->nextptr;
+            curNode->nextptr = prevNode;
+
+            prevNode = curNode;
+            curNode = stnode;
+        }
+        stnode = prevNode; //convert the last node as head
+    }
+}
+
+void displayList()
+{
+    struct node *tmp;
+    if(stnode == NULL)
+    {
+        printf(" No data found in the list.");
+    }
+    else
+    {
+        tmp = stnode;
+        while(tmp != NULL)
+        {
+            printf(" Data = %d\n", tmp->num);   // prints the data of current node
+            tmp = tmp->nextptr;                 // advances the position of current node
+        }
+    }
+}
+
